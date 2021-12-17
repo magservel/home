44a67a83a3cac60256d574621758bf081cda83c9
magali <mag.servel@gmail.com>
2021-12-17 16:01:14 +0100

    +Add wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
-from app import app
-
-if __name__ == "__main__":
-    app.run()
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works'.encode('utf-8'),
+            "</title><b>It works! magous test</b><br /><br />This is the server's ".encode('utf-8'),
+            'default wsgi.py.'.encode('utf-8')]
