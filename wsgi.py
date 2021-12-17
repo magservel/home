4a9ca72cb86e44284656bb2a89d105939c8da612
magali <mag.servel@gmail.com>
2021-12-17 16:07:41 +0100

    +test wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works'.encode('utf-8'),
-            "</title><b>It works! magous test</b><br /><br />This is the server's ".encode('utf-8'),
-            'default wsgi.py.'.encode('utf-8')]
+# def application(env, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+#     return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works'.encode('utf-8'),
+#             "</title><b>It works! magous test</b><br /><br />This is the server's ".encode('utf-8'),
+#             'default wsgi.py.'.encode('utf-8')]
+
+from app import app as application
