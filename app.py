1b2bf9e9acea8dc97300f8e58b50d35704c45920
magali <mag.servel@gmail.com>
2021-12-20 22:29:57 +0100

    create header

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__,
+            static_folder='src/static/',
+            template_folder='src/templates/')
 
 
 @app.route("/")
 def home():
-    return "App test"
+    return render_template("index.html")
