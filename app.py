2bb9275da318b1e84dc515e0a81aa49ea1bead04
magali <mag.servel@gmail.com>
2021-12-21 13:19:43 +0100

    +Refactor folder organization

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__,
-            static_folder='src/static/',
-            template_folder='src/templates/')
+app = Flask(__name__)
 
 
 @app.route("/")
